589f51efde9576043990466455fb8a515eed812d
Zachary M Waxman <zwaxman@gmail.com>
2019-07-16 19:58:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ var height = "74"
 if (name==="Susan"){
   console.log("The name is correct")
 }
+else{
+  console.log("Expected the name to be Susan")
+}
 
 // Don't worry about this
 module.exports = { name, height
